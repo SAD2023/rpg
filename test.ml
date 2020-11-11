5bb81baff036ad6c91b4fab0a482b39725facec7
Nicola Tysall <nct35@cornell.edu>
2020-11-10 21:11:32 -0500

    Fixed some errors in test

--- test.ml
+++ test.ml
@@ -23,35 +23,35 @@ let friend_a = Friend.make_friend "kevin" 5 5
 let friend_b = Friend.make_friend "joe" 5 5
 let friend_c = Friend.make_friend "bob" 5 5
 
-let student_a = Student.make_student "student" 18 0.0 0.0 0.0 0.0 0.0 []
-let student_b = Student.make_student "student" 18 4.0 0.0 0.0 0.0 0.0 []
-let student_c = Student.make_student "student" 18 4.0 0.0 0.0 0.0 0.0 [friend_a]
-let student_d = Student.make_student "student" 18 10.0 3.0 8.0 40.0 10.5 [friend_a; friend_b]
+let student_a = Student.make_student "student" 18 0.0 0.0 0.0 0.0 0.0 [] []
+let student_b = Student.make_student "student" 18 4.0 0.0 0.0 0.0 0.0 [] []
+let student_c = Student.make_student "student" 18 4.0 0.0 0.0 0.0 0.0 [friend_a] []
+let student_d = Student.make_student "student" 18 10.0 3.0 8.0 40.0 10.5 [friend_a; friend_b] []
 
 
-let update_student_test name student morality gpa social_life health brbs friends expected_output =
-  name >:: (fun _ -> assert_equal expected_output (Student.update_student student morality gpa social_life health brbs friends) (*~printer: Student.print_characteristics)*))
+let update_student_test name student morality gpa social_life health brbs friends dec expected_output =
+  name >:: (fun _ -> assert_equal expected_output (Student.update_student student morality gpa social_life health brbs friends dec) (*~printer: Student.print_characteristics)*))
 
 (**Mohammed, Nicola *)
 let student_tests = [
-  update_student_test "Changing one attribute (morality)" student_a 4.0 0.0 0.0 0.0 0.0 [] student_b;
-  update_student_test "Testing adding a friend" student_b 0.0 0.0 0.0 0.0 0.0 [friend_a] student_c;
-  update_student_test "Changing lots of attributes" student_c 6.0 3.0 8.0 40.0 10.5 [friend_b] student_d;
+  update_student_test "Changing one attribute (morality)" student_a 4.0 0.0 0.0 0.0 0.0 [] "" student_b;
+  update_student_test "Testing adding a friend" student_b 0.0 0.0 0.0 0.0 0.0 [friend_a] "" student_c;
+  update_student_test "Changing lots of attributes" student_c 6.0 3.0 8.0 40.0 10.5 [friend_b] "" student_d;
 ]
 
 
 
-let return_consequences_test name decision choices expected_output  =
-  name >:: fun ctxt -> assert_equal (cmp_set_like_lists (expected_output) (Scenario.return_consequences decision choices)) true
+let return_consequences_test name decision choices player expected_output  =
+  name >:: fun ctxt -> assert_equal (cmp_set_like_lists (expected_output) (Scenario.return_consequences decision choices player)) true
 
-let return_consequences_error_test name decision choices expected_output  =
-  name >:: fun ctxt -> assert_raises expected_output (fun () -> (Scenario.return_consequences decision choices))
+let return_consequences_error_test name decision choices player expected_output  =
+  name >:: fun ctxt -> assert_raises expected_output (fun () -> (Scenario.return_consequences decision choices player))
 
 (**Sadman, Lirinda *)
 let scenario_tests = [
-  return_consequences_test "first choice" "single" ["double"; "single"] [("gpa", 0.2)];
+  return_consequences_test "first choice" "single" ["double"; "single"] student_a [("gpa", 0.2)];
 
-  return_consequences_error_test "choice not in choice list" "outside" ["double"; "single"] (InvalidInput "outside");
+  return_consequences_error_test "choice not in choice list" "outside" ["double"; "single"] student_a (InvalidInput "outside");
 
 
 ]
