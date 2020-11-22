4b3296269e88bb9eb2f1b766cb3a45af8d57c718
lar332 <lar332@cornell.edu>
2020-11-22 18:37:40 -0500

    spec comments

--- minigames.ml
+++ minigames.ml
@@ -12,12 +12,15 @@ type minigame
 
 open Stdlib
 
-(** [remove_char str index] removes the string str without *)
+(** [remove_char str index] takes in a string and index value and removes
+    a character in order to switch up the ordering of a word. *)
 let remove_char str index =
   let sub2_start = String.sub str (index + 1) in
   let sub2_length = String.length(str) - (index + 1) in 
   String.sub str 0 index ^ sub2_start sub2_length
 
+(** [scramble_word] takes in a string word and scrambles the given word
+    with the remove_char helper function. *)
 let rec scramble_word (word : string) (result : string)=
   if String.length word = 0 then result else
     let index = (Random.int (String.length word)) in
