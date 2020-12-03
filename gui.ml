ccabc7513542d223d25f17a6d0daaea66f759d77
Mohammed Ullah <mu83@cornell.edu>
2020-12-02 23:34:14 -0500

    Made background black because looked cooler

--- gui.ml
+++ gui.ml
@@ -7,6 +7,7 @@ let print_graph_choices choices =
     y.contents <- y.contents - 50;
     let x1 = x.contents in let y1 = y.contents in 
     Graphics.moveto x1 y1;
+    Graphics.set_color Graphics.black;
     Graphics.draw_ellipse x1 y1 200 20;
     Graphics.set_color Graphics.green;
     Graphics.fill_ellipse x1 y1 200 20;
@@ -15,7 +16,6 @@ let print_graph_choices choices =
     Graphics.moveto (x1 - (choice_size / 2)) (y1 - 5);
     Graphics.set_text_size 100;
     Graphics.draw_string (List.nth choices i);
-
   done
 
 let make_graph word = 
@@ -25,6 +25,8 @@ let make_graph word =
 
 let make_graph_scenario prompt choices= 
   Graphics.clear_graph ();
+  Graphics.fill_rect 0 0 (Graphics.size_x ()) (Graphics.size_y ());
+
   let x = ref 50 in 
   let y = ref 400 in 
   Graphics.moveto 50 400;
@@ -32,6 +34,7 @@ let make_graph_scenario prompt choices=
   for i= 0 to List.length prompt_list - 1 do
     y.contents <- y.contents - 15;
     Graphics.moveto x.contents y.contents;
+    Graphics.set_color Graphics.white;
     Graphics.draw_string (List.nth prompt_list i);
   done;
   print_graph_choices choices
